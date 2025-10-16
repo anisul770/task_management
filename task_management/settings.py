9393cbf316aed62e540226150b61a6a99c67368c
Anisul Haque <anisulhaque773@gmail.com>
2025-10-16 16:15:50 +0600

    Add STATIC_ROOT for Render deployment

--- task_management/settings.py
+++ task_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
+import dj_database_url,os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -101,9 +101,6 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 #     )
 # }
 
-import dj_database_url
-import os
-
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
@@ -161,7 +158,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR /'static'
 ]
